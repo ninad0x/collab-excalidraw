559fc4badf848caca981d0074f6d3415a8f541a3
Ninad09 <ninadgaonkar234@gmail.com>
2025-07-30 23:17:06 +0530

    modified index.ts

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,6 +1,6 @@
 import { WebSocket, WebSocketServer } from "ws";
 import jwt, { JwtPayload } from "jsonwebtoken"
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/backend-common/config"
 
 const wss = new WebSocketServer({ port: 8080 });
 
@@ -17,7 +17,13 @@ wss.on('connection', function connection(ws: WebSocket, request) {
 
     const decoded = jwt.verify(token, JWT_SECRET)
 
-    if (!decoded || !(decoded as JwtPayload).userId) {
+    
+    if (typeof decoded === "string") {
+        ws.close()
+        return
+    }
+
+    if (!decoded || !decoded.userId) {
         ws.close()
         return
     }
