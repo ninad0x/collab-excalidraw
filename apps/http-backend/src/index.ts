73911e4e42f577ae05c0e1329b29fa31479fb607
Ninad09 <ninadgaonkar234@gmail.com>
2025-08-01 23:10:38 +0530

    index.ts - added db calls middleware.ts - import updated to backend common

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,32 +1,16 @@
 import express from "express"
-import jwt from "jsonwebtoken"
+import jwt, { JwtPayload } from "jsonwebtoken"
 import { JWT_SECRET } from "@repo/backend-common/config"
 import { userMiddleware } from "./middleware"
-import { CreateRoomSchema, CreateUserSchema } from "@repo/common/types"
+import { CreateRoomSchema, CreateUserSchema, SigninSchema } from "@repo/common/types"
+import { prismaClient }  from "@repo/db/client"
 
 
 const app = express()
 app.use(express.json())
 
 
-app.post("/signup", (req, res) => {
-
-    const { success, data, error } = CreateUserSchema.safeParse(req.body)
-
-    if (!success) {
-        return res.json({
-            message: error.issues[0]?.message
-        })
-    }
-
-    return res.json({
-        message: "Signup success"
-    })
-    
-})
-
-
-app.post("/signin", (req, res) => {
+app.post("/signup", async (req, res) => {
 
     const { success, data, error } = CreateUserSchema.safeParse(req.body)
 
@@ -35,12 +19,56 @@ app.post("/signin", (req, res) => {
             message: error.issues.map(e => e.message)
         })
     }
+    
+    try {
+        const user = await prismaClient.user.create({
+            data : {
+                email: data.email,
+                password: data.password,   // TODO : hash pasword
+                name: data.name
+            }
+        })
 
-    const userId = 1
+        return res.json({
+            userId: user.id
+        })
+    
+    } catch (e) {
+        return res.status(403).json({
+            message: "email taken"
+        })
+    }
+})
+
+
+app.post("/signin", async (req, res) => {
+
+    const { success, data, error } = SigninSchema.safeParse(req.body)
+
+    if (!success) {
+        return res.json({
+            message: error.issues.map(e => e.message)
+        })
+    }
+
+    // TODO : compare hash pasword
+
+    const user = await prismaClient.user.findFirst({
+        where: {
+            email: data.email,
+            password: data.password
+        }
+    })
+
+    if (!user) {
+        return res.json({
+            message: "User does not exist, pls signup"
+        })
+    }
+
     const token = jwt.sign({
-        userId
+        userId: user?.id
     }, JWT_SECRET)
-
 
     return res.json({
         token
@@ -49,7 +77,7 @@ app.post("/signin", (req, res) => {
 })
 
 
-app.post("/room", userMiddleware, (req, res) => {
+app.post("/room", userMiddleware, async (req, res) => {
 
     const { success, data, error } = CreateRoomSchema.safeParse(req.body)
 
@@ -59,9 +87,26 @@ app.post("/room", userMiddleware, (req, res) => {
         })
     }
 
-    return res.json({
-        roomId: 123
-    })
+    // @ts-ignore
+    const userId = req.userId
+
+    try {
+        const room = await prismaClient.room.create({
+            data: {
+                slug: data.name,
+                adminId: userId
+            }
+        })
+    
+        return res.json({
+            roomId: room.id
+        })
+        
+    } catch (error) {
+        res.status(411).json({
+            message: "Room name must be unique"
+        })
+    }
     
 })
 
--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/backend-common/config"
 
 
 export function userMiddleware(req: Request, res: Response, next: NextFunction) {
