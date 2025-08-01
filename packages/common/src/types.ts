e9b7968bad4a0384d98bf1d35e410793bc197b58
Ninad09 <ninadgaonkar234@gmail.com>
2025-08-01 23:02:34 +0530

    modified schema

--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -1,14 +1,14 @@
 import { z } from "zod"
 
 export const CreateUserSchema = z.object({
-    username: z.string().min(3).max(20),
-    password: z.string().min(3).max(20),
+    email: z.email(),
+    password: z.string().min(6).max(20),
     name: z.string()
 })
 
 export const SigninSchema = z.object({
-    username: z.string().min(3).max(20),
-    password: z.string().min(3).max(20),
+    email: z.email(),
+    password: z.string().min(6).max(20),
 })
 
 export const CreateRoomSchema = z.object({
