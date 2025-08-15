121ae16c72a0b94fb68b11460f8005db93729359
Ninad09 <ninadgaonkar234@gmail.com>
2025-08-15 20:38:28 +0530

    added button props

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -3,16 +3,25 @@
 import { ReactNode } from "react";
 
 interface ButtonProps {
-  children: ReactNode;
+  variant: "primary" | "secondary" | "outline"
+  children?: ReactNode;
   className?: string;
-  appName: string;
+  appName?: string;
+  size: "lg" | "sm";
+  onClick?: () => void
 }
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
+const variantStyle = {
+  primary: "bg-blue-500",
+  secondary: "bg-sky-500",
+  outline: "border-1"
+}
+
+export const Button = ({ children, className, appName, size, onClick, variant }: ButtonProps) => {
   return (
     <button
-      className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
+      className={`${className} ${size} ${variantStyle[variant]}`}
+      onClick={onClick}
     >
       {children}
     </button>
